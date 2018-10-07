aeb41dbbed71aa0f04d4b0a15a263d1ffc536a45
Jonas Schnelli <dev@jonasschnelli.ch>
2018-10-07 16:38:16 +0200

    some bugfixed and copy'n'pase function

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ ini_set('display_errors', 1);
     for ($i=0;$i<$blk_count;$i++) {
       array_push($lastblocks, getBlockJSON($hash));
       $hash = $lastblocks[count($lastblocks)-1]->previousblockhash;
-      $age_in_s = $NOW - $lastblocks[count($lastblocks)-1]->mediantime;
+      $age_in_s = $NOW - $lastblocks[count($lastblocks)-1]->time;
       if ($age_in_s > 3600) {
         $lastblocks[count($lastblocks)-1]->age = gmdate("G", $age_in_s)." hours, ".gmdate("i", $age_in_s)." min";
       }
@@ -101,7 +101,7 @@ ini_set('display_errors', 1);
     if (isset($json->blockhash)) { $json->blockheader = getBlockHeader($json->blockhash); }
     $missing_prevout = false;
     foreach($json->vin as &$vin) {
-      if ($vin->txid) {
+      if (isset($vin->txid)) {
         $prevout = httpFetch("rest/tx/".$vin->txid.".json");
         if(isset($prevout)) {
           $vin->prevout = $prevout->vout[$vin->vout];
@@ -111,7 +111,9 @@ ini_set('display_errors', 1);
     if (!$missing_prevout) {
       $total_in = 0;
       foreach($json->vin as &$vin) {
-        $total_in += $vin->prevout->value;
+        if (isset($vin->prevout)) {
+          $total_in += $vin->prevout->value;
+        }
       }
       $json->total_in = $total_in;
     }
@@ -125,6 +127,9 @@ ini_set('display_errors', 1);
     }
     $json->total_out = $total_out;
     if (!$missing_prevout) { $json->fee = round($total_in-$total_out, 8); }
+    if ($total_in == 0) {
+      $json->fee = 0;
+    }
     return $json;
   }
   function txlink($hash, $vout="") {
@@ -161,6 +166,7 @@ ini_set('display_errors', 1);
   <title><?php echo $HTMLTITLE; ?></title>
   <meta name="author" content="">
   <meta name="description" content="">
+<?php if($VIEWTYPE == ViewType::Overview): ?>  <meta http-equiv="refresh" content="60"/><?php endif;?>
 
 <!-- IE Edge Meta Tag -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -170,12 +176,6 @@ ini_set('display_errors', 1);
 
 <!-- Minified CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-<!-- Optional Theme -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
 
 <style>
   body {
@@ -293,12 +293,12 @@ span.text-muted.spent {
           <td><?php echo $block->height; ?></td>
         </tr>
         <tr>
-          <th>Hash</th>
-          <td><?php echo $block->hash; ?></td>
+          <th>Hash <a class="copyicon" data-clipboard-target="#blockhash"><img src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.0.0/svg/clippy.svg" /></a></th>
+          <td class="text-truncate"><span id="blockhash"><?php echo $block->hash; ?></span></td>
         </tr>
         <tr>
           <th>Time</th>
-          <td><?php echo date("Y-m-d H:i:s", $block->mediantime); ?></td>
+          <td><?php echo date("Y-m-d H:i:s", $block->time); ?></td>
         </tr>
         <tr>
           <th>Transactions</th>
@@ -314,11 +314,11 @@ span.text-muted.spent {
         </tr>
         <tr>
           <th>Previous block</th>
-          <td><a href="<?php echo blocklink($block->previousblockhash); ?>"><?php echo $block->previousblockhash; ?></a></td>
+          <td class="text-truncate"><a href="<?php echo blocklink($block->previousblockhash); ?>"><?php echo $block->previousblockhash; ?></a></td>
         </tr>
         <tr>
           <th>Next block</th>
-          <td><?php if(isset($block->nextblockhash)): ?><a href="<?php echo blocklink($block->nextblockhash); ?>"><?php echo $block->nextblockhash; ?></a><?php endif; ?></td>
+          <td class="text-truncate"><?php if(isset($block->nextblockhash)): ?><a href="<?php echo blocklink($block->nextblockhash); ?>"><?php echo $block->nextblockhash; ?></a><?php endif; ?></td>
         </tr>
         </tbody>
       </table>
@@ -328,8 +328,8 @@ span.text-muted.spent {
 <?php if(isset($block->tx)): ?>
       <ul class="list-group">
 <?php $i = 0; foreach($block->tx as $tx): $i++?>
-        <li class="list-group-item d-flex justify-content-between lh-condensed <?php if($tx == $_REQUEST['txid']) echo "active"; ?>">
-          <div><a href="<?php echo txlink($tx); ?>"><?php echo $tx; ?></a></div>
+        <li class="list-group-item d-flex justify-content-between lh-condensed <?php if(isset($_REQUEST['txid']) && $tx == $_REQUEST['txid']) echo "active"; ?>">
+          <div class="text-truncate"><a href="<?php echo txlink($tx); ?>"><?php echo $tx; ?></a></div>
           <span class="text-muted"><?php echo $i; ?></span>
         </li>
 <?php endforeach;
@@ -347,8 +347,8 @@ else:?>
     <table class="table table-bordered table-hover table-striped bg-white">
       <tbody>
       <tr>
-        <th>TXID</th>
-        <td><?php echo $tx->txid; ?></td>
+        <th>txid <a class="copyicon" data-clipboard-target="#txid"><img src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.0.0/svg/clippy.svg" /></a></th>
+        <td><span id="txid"><?php echo $tx->txid; ?></a></td>
       </tr>
       <tr>
         <th>Hash</th>
@@ -382,14 +382,16 @@ else:?>
         <th>Total Outputs</th>
         <td><?php if (isset($tx->total_out)) echo btcamount($tx->total_out); else echo "N/A"; ?></td>
       </tr>
+      <?php if($tx->total_in > 0): ?>
       <tr>
         <th>Fee</th>
         <td><?php if (isset($tx->fee)) echo btcamount($tx->fee); else echo "N/A"; ?></td>
       </tr>
       <tr>
         <th>Feerate</th>
-        <td><?php if (isset($tx->fee)) echo $tx->fee*100000000/$tx->vsize; else echo "N/A"; ?></td>
+        <td><?php if (isset($tx->fee)) echo round($tx->fee*100000000/$tx->size, 3); else echo "N/A"; ?> sat/B</td>
       </tr>
+      <?php endif; ?>
       </tbody>
     </table>
   </div>
@@ -401,8 +403,10 @@ else:?>
         <li class="list-group-item d-flex justify-content-between lh-condensed">
 <?php if (isset($vin->prevout)):?>
             <div><a href="<?php echo txlink($vin->txid, $vin->vout); ?>"><?php echo $vin->prevout->scriptPubKey->addresses[0]; ?></a></div><span class="text-muted"><?php echo btcamount($vin->prevout->value); ?></span>
-<?php else: ?>
+<?php elseif(isset($vin->txid)): ?>
             <div><a href="<?php echo txlink($vin->txid, $vin->vout); ?>"><?php echo $vin->txid; ?></a></div>
+<?php else: ?>
+            <div>Coinbase</div>
 <?php endif; ?>
         </li>
 <?php endforeach;?>
@@ -413,7 +417,7 @@ else:?>
       <ul class="list-group">
 <?php foreach($tx->vout as $vout): ?>
         <li class="list-group-item lh-condensed<?php if (isset($_REQUEST['n']) && $_REQUEST['n'] == $vout->n) echo " active"; ?>">
-          <span class="badge badge-primary"><?php echo $vout->scriptPubKey->type; ?></span> <?php if (isset($vout->is_unspent)): ?><span class="badge badge-success">unspent</span><?php else: ?><span class="badge badge-warning">spent</span><?php endif; ?><div><h6><?php if (isset($vout->scriptPubKey->addresses)) echo $vout->scriptPubKey->addresses[0]; ?></h6><span class="text-muted<?php if (!isset($vout->is_unspent)) echo " spent"; ?>"><?php echo btcamount($vout->value); ?></span></div>
+          <span class="badge badge-primary"><?php echo $vout->scriptPubKey->type; ?></span> <?php if (isset($vout->is_unspent)): ?><span class="badge badge-success">unspent</span><?php else: ?><span class="badge badge-warning">spent</span><?php endif; ?><div><h6><?php if (isset($vout->scriptPubKey->addresses)) echo $vout->scriptPubKey->addresses[0]; ?></h6><span class="text-muted<?php if (!isset($vout->is_unspent)) echo " spent"; ?>"><?php if ($vout->scriptPubKey->type != "nulldata") { echo btcamount($vout->value); } else { echo "<span style=\"font-size:0.7em;color:black !important;\">".$vout->scriptPubKey->asm."</span>"; }; ?></span></div>
         </li>
 <?php endforeach;?>
       </ul>
@@ -453,5 +457,9 @@ else:?>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
+<script>
+    var clipboard = new ClipboardJS('.copyicon');
+    </script>
 </body>
 </html>
